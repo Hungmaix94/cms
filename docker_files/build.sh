1d2862fd9dc19f23d40a404e80afff62b31a54fa
bigfatpanda <phamhung.bk94@gmail.com>
2020-12-26 11:59:00 +0700

    first commit cms

---
+++ docker_files/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ ! -f ./.env ]]; then
+   echo "Not exist any .env file configuration"
+   exit
+fi
+#docker-compose -f services/proxy.yml up -d
+#docker-compose --compatibility -f services/adminer.yml up -d
+#docker-compose --compatibility -f services/elastic.yml up -d
+#docker-compose --compatibility -f services/ilead.yml up -d
+#docker-compose --compatibility -f services/icvs.yml up -d
+#docker-compose --compatibility -f services/icvs.tool.yml up -d
+# docker-compose --compatibility -f services/webhooks.yml up -d
+#docker-compose --compatibility -f services/cv_fresher.yml up -d
+docker-compose --compatibility -f services/cms.yml up -d
+docker-compose --compatibility -f services/nginx-proxy.yml up -d
